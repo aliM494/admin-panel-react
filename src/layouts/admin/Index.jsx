374ab60fe2ad0373a13850c18cde38e2f687c79d
aliM494 <smalim494.1810768950@gmail.com>
2022-06-02 22:01:09 +0430

    initials git for  project

---
+++ src/layouts/admin/Index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AdminContextContainer from "../../context/adminLayoutContext";
+import Content from "../../pages/Content";
+import Navbar from "./navbar/Index";
+import Sidebar from "./sidebar/Index";
+
+const Index = () => {
+  return (
+    <AdminContextContainer>
+      <div>
+        <Content />
+        <Navbar />
+        <Sidebar />
+      </div>
+    </AdminContextContainer>
+  );
+};
+
+export default Index;
